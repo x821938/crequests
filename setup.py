f621760f45dd23485622097f3229d0a3b4a89cda
Alex Jensen <x821938@offline.dk>
2020-06-20 07:40:42 +0200

    Added keywords to egg

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="crequests",
-    version="0.2.1",
+    version="0.2.2",
     author="Alex Skov Jensen",
     author_email="pydev@offline.dk",
     description="A lightweight cache wrapper for requests",
@@ -14,11 +14,15 @@ setuptools.setup(
     url="https://github.com/x821938/crequests",
     packages=["crequests"],
     license="MIT",
+    keywords=["requests", "cache", "scrape", "scraping", "persistent", "persistance"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Session",
+        "Intended Audience :: Developers",
     ],
     project_urls={
         "Documentation": "https://github.com/x821938/crequests",
